74b6e455ce1bbeed22c2e0b84a3143cac3ccae6f
kyun <839530159@qq.com>
2019-04-09 11:02:49 +0800

    删除工程

--- kettle-server/src/main/java/cn/telchina/kettleserver/ext/utils/SAPR3DatabaseMeta.java
+++
@@ -1,9 +0,0 @@
-package cn.telchina.kettleserver.ext.utils;
-
-public class SAPR3DatabaseMeta {
-
-    public static final String ATTRIBUTE_SAP_LANGUAGE = "SAPLanguage";
-    public static final String ATTRIBUTE_SAP_SYSTEM_NUMBER = "SAPSystemNumber";
-    public static final String ATTRIBUTE_SAP_CLIENT = "SAPClient";
-    
-}
